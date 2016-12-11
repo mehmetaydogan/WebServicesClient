71a4eb4e504ef82da88d575b222ea963bc8b881f
FatizAglif <fatiz.aglif@gmail.com>
2016-12-11 19:26:06 +0100

    Catégories Update-Delete-Get-Add-Filtre + HTML/CSS

--- public_html/js/libs/etc/initCategories.js
+++ public_html/js/libs/etc/initCategories.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
     $.getJSON('http://localhost:8080/biblio/webresources/categorie/', function(categories) {
-        console.log(categories);
+        //console.log(categories);
         /*
             Cette fonction est le controlleur de la vue 
             Elle assure la communication entre la vue et le modèle, une sorte de pont quoi! 
-        */    
+        */
         ko.applyBindings(new ViewModel(categories));
     });
+    $('#myTable').DataTable();
 });
 
 
--- public_html/js/models/viewModel.js
+++ public_html/js/models/viewModel.js
@@ -1,33 +1,84 @@
 var ViewModel = function (categories) {
     var self = this;
+    this.filter = ko.observable();
     /*
         Représente la liste des catégories
         La fonction prend la réponse obtenue du serveur en paramètre
         Ici nous supposons que vous avez chargé la liste des catégories
         ko.utils.arrayMap itère sur la collection et pour chaque objet trouvé, elle crée une instance de categorie 
-    */
+     */
     self.categories = ko.observableArray(ko.utils.arrayMap(categories, function (categorie) {
         return new Category(categorie);
     }));
     
+    self.currentFilter = ko.observable();
+    
+    self.filterCategories = ko.computed(function() {
+       if(!self.filter()) {
+           return self.categories();
+       } else {
+           return ko.utils.arrayFilter(self.categories(), function(categorie) {
+              var categorieNom = ko.toJS(categorie.nom);
+              var categorieDescription = ko.toJS(categorie.description);
+              return categorieNom.startsWith(self.filter()) || categorieDescription.startsWith(self.filter());
+           });
+       }
+    });
+    
+    self.books = function (categorie) {
+        document.location.href = "ListeLivres.html?categorie="+ko.toJS(categorie.id);
+    };
+    
+    self.add = function () {
+        self.categories.push({
+            id : "",
+            nom : "",
+            description : ""
+        });
+    };
+
     self.remove = function (categorie) {
         self.categories.remove(categorie);
         $.ajax({
-            url: [URI],
+            url: ["http://localhost:8080/biblio/webresources/categorie/"+ko.toJS(categorie.id)],
             type: "DELETE",
             contentType: "application/json",
             headers: {
-                Accept: "application/json"
+                Accept : "application/json"
             }
-        }).success(function (data, status, jq) {
-            // alert(status);
-            self.categories.remove(categorie);
-        }).error(function (jq, status, error) {
-            $(".error").text(JSON.stringify(status + " " + error));
         });
     };
-    
     self.update = function (categorie) {
-        //Effectuez votre requête AJAX ici
+        // Si l'id de la Categorie n'est pas reconnu alors on l'ajout Sinon on le met a jour.
+        if(ko.toJS(categorie.id) === "") {
+            //console.log(self.categories[ko.toJS(categorie.id)])
+            $.ajax({
+                url: ["http://localhost:8080/biblio/webresources/categorie/"],
+                type: "POST",
+                contentType: "application/x-www-form-urlencoded",
+                headers: {
+                    Accept : "application/json"
+                },
+                data: {
+                    nom: ko.toJS(categorie.nom),
+                    description: ko.toJS(categorie.description)
+                }
+            });
+            window.location.reload();
+        } else {
+            self.categories.replace(self.categories[ko.toJS(categorie.id)], categorie);
+            $.ajax({
+                url: ["http://localhost:8080/biblio/webresources/categorie/"+ko.toJS(categorie.id)],
+                type: "PUT",
+                contentType: "application/x-www-form-urlencoded",
+                headers: {
+                    Accept : "application/json"
+                },
+                data: {
+                    nom: ko.toJS(categorie.nom),
+                    description: ko.toJS(categorie.description)
+                }
+            });
+        }
     };
 };
